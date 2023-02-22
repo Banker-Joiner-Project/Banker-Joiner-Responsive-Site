da7c3dd508c409f1f49b90f2b8f7e58595a0be6a
Austin Joiner <austin.j.joiner1@gmail.com>
2023-02-22 13:19:57 -0500

    added a bonus to the project.

---
+++ js/Just-a-little-fun.js
@@ -0,0 +1,24 @@
+var keyCode = [37, 39, 37, 39, 13];
+var ind = 0; //starts index at 0 so we can increment it up
+$(document).keyup(function(event) {
+    console.log(event.keyCode);
+    if (event.which === keyCode[ind]) {
+        ind++;  // adds 1 to index if input code correctly making it to where it will be 11 in total
+    } else {
+        ind = 0; // this it to reset the code if typed wrong
+    }
+    if (ind === keyCode.length) { // once ind = 11 it will successfully pass through this if statement and reset it to zero
+        ind = 0;
+        alert('Thank You!');
+
+        var fireAudio = $('#fireworksAudio');
+        fireAudio[0].play();
+        var cheerAudio = $('#cheersAudio');
+        setTimeout(function () {
+            cheerAudio[0].play();
+        }, 5000);
+    }
+
+
+
+});
